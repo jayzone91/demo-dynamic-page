46d4db6ca4d2058ccb834bc9b5589e51f6d16dbc
jayzone91 <register@j4yz0n3.de>
2025-03-05 10:58:12 +0100

    addded header

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router";
+import { Header } from "./components/header";
 
 export function RootLayout() {
   return (
-    <>
+    <div className="bg-white dark:bg-gray-800 min-h-[100vh]">
+      <Header />
       <Outlet />
-    </>
+    </div>
   );
 }
